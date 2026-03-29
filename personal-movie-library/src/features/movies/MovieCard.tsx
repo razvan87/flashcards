5a0ec5094fd8056895628956f3b492920018323e
Razvan Bran <razvan.bran@gmail.com>
2026-03-29 19:52:52 +0300

    add toggle feature

--- personal-movie-library/src/features/movies/MovieCard.tsx
+++ personal-movie-library/src/features/movies/MovieCard.tsx
@@ -2,16 +2,37 @@ import type { Movie } from "./types";
 
 type Props = { 
     movie: Movie;
+    onToggleWatched: (updatedMovie: Movie) => void;
 }
 
-function MovieCard({ movie }: Props) {
+function MovieCard({ movie, onToggleWatched }: Props) {
+
+  const handleToggleWatched = () => {
+    fetch(`/api/movies/${movie.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        watched: !movie.watched,
+      })
+    })
+    .then((res) => res.json())
+    .then((updatedMovie) => {
+      console.log("Updated Movie:", updatedMovie);
+      onToggleWatched(updatedMovie);
+    })
+    .catch(console.error);
+  };
+
   return (
     <div>
       <h2>{movie.title}</h2>
       <p>Director: {movie.director}</p>
       <p>Year: {movie.year}</p>
       <p>Watched: {movie.watched ? "Yes" : "No"}</p>
-    </div>
+      <button onClick={handleToggleWatched}> Mark as {movie.watched ? "Unwatched" : "Watched"}</button>
+    </div>    
   );
 }
 
--- personal-movie-library/src/features/movies/MovieList.tsx
+++ personal-movie-library/src/features/movies/MovieList.tsx
@@ -11,13 +11,22 @@ export function MovieList() {
     .then((data: Movie[]) => setMovies(data))
     .catch(console.error);
   }, []);
+
+  const handleToggleWatched = (updatedMovie: Movie) => {
+    setMovies((prevMovies) => {
+      if (!prevMovies) return prevMovies;
+      return prevMovies.map((movie) =>
+        movie.id === updatedMovie.id ? updatedMovie : movie
+      );
+    });
+  }
   
   
   return (
     <div>
       <h1>Movie List:</h1>
         {!movies && <p>Loading...</p>}
-        {movies?.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+        {movies?.map(movie => <MovieCard key={movie.id} movie={movie} onToggleWatched={handleToggleWatched} />)}
     </div>
   );
 }
